0d69bc5572e19fed42257c2769a8919e0eafa536
sanyam-jain2404 <jainsanyam981@gmail.com>
2026-04-16 14:26:32 +0530

    Global Context

---
+++ context_demo/src/App.jsx
@@ -0,0 +1,29 @@
+import { useContext } from "react";
+import{GlobalContext} from './context';
+
+const App=()=>{
+
+  /*const {theme,toggleTheme}=useContext(ThemeContext);
+  const {user,setUser}=useContext(UserContext);
+  const {lang,setLang}=useContext(LangContext);*/
+
+     const {state,dispatch}=useContext(GlobalContext);
+    const {theme,user,lang}=state;
+
+  return(
+    <div style={{background:theme==="light"?"#fff":"#333"}}>
+      <p>Theme:{theme}</p>
+      <p>User:{user.name}</p>
+      <p>Lang:{lang}</p>
+
+      <button onClick={()=>dispatch({type:"TOGGLE_THEME"})}>ThemeChange</button>
+      <button onClick={()=>dispatch({type:"USE_LANG"})}>en</button>
+      <button onClick={()=>dispatch({type:"USE_LANG"})}>fr</button>
+
+
+
+    </div>
+  )
+}
+
+export default App;
